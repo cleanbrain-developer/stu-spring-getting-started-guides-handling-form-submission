d075f87dd5d0a3b190e207f048b665317628d42a
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-16 20:18:05 +0900

    first commit

---
+++ src/main/java/com/example/StuSpringGettingStartedGuidesHandlingFormSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuSpringGettingStartedGuidesHandlingFormSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuSpringGettingStartedGuidesHandlingFormSubmissionApplication.class, args);
+	}
+
+}
